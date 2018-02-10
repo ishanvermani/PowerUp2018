f80f5e07eae4f1cd5691bc5adc9eb6969d40f818
Rundle-Shared <srhigh@rundle.ab.ca>
2018-02-10 15:58:27 -0700

    Update ArmUp.java

--- PowerUp2018/src/org/usfirst/frc/team6351/robot/commands/ArmUp.java
+++ PowerUp2018/src/org/usfirst/frc/team6351/robot/commands/ArmUp.java
@@ -3,11 +3,11 @@ package org.usfirst.frc.team6351.robot.commands;
 import org.usfirst.frc.team6351.robot.Robot;
 import org.usfirst.frc.team6351.robot.RobotMap;
 
-public class ArmUp extends Command {
+public class ArmDown extends Command {
 	public UpperArm() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-		requires(Robot.Winch);
+		requires(Robot.winch);
 	}
 	
     // Called just before this Command runs the first time
@@ -16,7 +16,7 @@ public class ArmUp extends Command {
 	
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.Winch.oneDirection();
+    	Robot.winch.oneDirection();
     }
 	
     // Make this return true when this Command no longer needs to run execute()
@@ -26,7 +26,7 @@ public class ArmUp extends Command {
 	
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.Winch.stop();
+    	Robot.winch.stop();
     }
 	
     // Called when another command which requires one or more of the same
@@ -34,4 +34,4 @@ public class ArmUp extends Command {
     protected void interrupted() {
     }
     
-}
+
