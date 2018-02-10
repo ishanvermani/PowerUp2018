a412552a1387613ed6439f403ed36e3488e7c0b8
Rundle-Shared <srhigh@rundle.ab.ca>
2018-02-10 14:39:12 -0700

    Create ArmUp.java

---
+++ PowerUp2018/src/org/usfirst/frc/team6351/robot/commands/ArmUp.java
@@ -0,0 +1,38 @@
+package org.usfirst.frc.team6351.robot.commands;
+
+import org.usfirst.frc.team6351.robot.Robot;
+import org.usfirst.frc.team6351.robot.RobotMap;
+
+public class ArmUp extends Command {
+	public UpperArm() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+		
+		requires(Robot.Winch);
+	}
+    // Called just before this Command runs the first time
+    protected void initialize() {	
+    }
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.Winch.oneDirection();
+    }
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+ // Note to self: figure out how to do this
+        if (driverA != pressed) {
+        	return true;
+        } else {
+        	return false;
+        }
+    }
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.Winch.stop();
+    }
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+    
+}
