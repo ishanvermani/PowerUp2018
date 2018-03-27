bc776aca5ddd5a6e13b612458e79dbf388781ebe
Davis Carlson <bigdavisc@shaw.ca>
2018-03-27 11:25:31 -0600

    Added command to drive backwards autonomously

---
+++ PowerUp2018/src/org/usfirst/frc/team6351/robot/auto/commands/Auto_DriveBackwards.java
@@ -0,0 +1,104 @@
+package org.usfirst.frc.team6351.robot.auto.commands;
+
+import org.usfirst.frc.team6351.robot.Robot;
+import org.usfirst.frc.team6351.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+/**
+ *
+ */
+public class Auto_DriveBackwards extends Command {
+	
+	double spd, tme, dst;
+	boolean encoderDrive;
+	
+    public Auto_DriveBackwards(double speed, double time, double distance) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.driveTrain);
+    	requires(Robot.sensors);
+    	spd = speed;
+    	
+    	if (time == 0.0) {
+    		encoderDrive = true;
+    		dst = distance;
+    	} else {
+        	tme = time;
+    	}
+    	
+    }
+    
+    public Auto_DriveBackwards(double distance) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	this(0.4, 0.0, distance);
+    	
+    }
+    
+    public Auto_DriveBackwards(double distance, double speed) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	this(speed, 0.0, distance);
+    	
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	if (encoderDrive == true) {
+    		Robot.sensors.driveEncoderLeft.reset();
+    		Robot.driveTrain.setLeft(spd*RobotMap.Curve_Reduction_Factor*(-1));
+    		Robot.driveTrain.setRight((spd));
+    		
+    		
+    	} else {
+    		Robot.driveTrain.setLeft(spd*RobotMap.Curve_Reduction_Factor*(-1));
+    		Robot.driveTrain.setRight((spd));
+    		Timer.delay(tme);
+    	}
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	if (encoderDrive == true) {
+    		double currentDistance = Robot.sensors.getDriveEncoderDistance();
+    		if (currentDistance <= dst + 3) {
+    			Robot.driveTrain.setLeft(0.1);
+        		Robot.driveTrain.setRight(-0.1);
+        		Timer.delay(0.4);
+    			Robot.driveTrain.setLeft(0.0);
+        		Robot.driveTrain.setRight(0.0);
+    		}
+    	} else {
+    		Robot.driveTrain.setLeft(0.0);
+    		Robot.driveTrain.setRight(0.0);
+    	}
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	if (encoderDrive == true) {
+    		double currentDistance = Robot.sensors.getDriveEncoderDistance();
+    		if (currentDistance <= dst + 3) {
+    			return true;
+    		} else {
+    			return false;
+    		}
+    	} else {
+    		return true;
+    	}
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    		Robot.driveTrain.setLeft(0.0);
+		Robot.driveTrain.setRight(0.0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    		Robot.driveTrain.setLeft(0.0);
+		Robot.driveTrain.setRight(0.0);
+    }
+}
